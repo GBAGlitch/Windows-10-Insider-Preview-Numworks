8cfece21e65ff8ab4033dc760b19310adc7f4e0a
GBAGlitch <59638234+GBAGlitch@users.noreply.github.com>
2020-01-08 07:30:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
 
+#made on Numworks Calculator 
+#made by GBAGlitch
+
+from time import*
+from math import*
+from kandinsky import*
+from random import*
+from turtle import*
+b = color(255,255,255)
+ta = ("")
+tb = ("")
+tc = ("")
+td = ("")
+te = ("")
+tf = ("")
+tg = ("")
+th = ("")
+ti = ("")
+tj = ("")
+#Microsoft Windows 10
+def bootui(a)
+  if a == 1 :
+    fill_rect(0,0,640,480,(0,0,0))
+    fill_rect(120,50,40,40,(128,128,255))
+    fill_rect(120,93,40,40,(128,128,255))
+    fill_rect(163,40,50,50,(128,128,255))
+    fill_rect(163,94,50,50,(128,128,255))
+    for i in range(10) :
+      set_pixel
+      set_pixel
+      set_pixel
+      set_pixel
+      sleep(0.1)
+      set_pixel
+      set_pixel
+      set_pixel
+      set_pixel
+      sleep(0.1)
+      set_pixel
+      set_pixel
+      set_pixel
+      set_pixel
+      sleep(0.1)
+      set_pixel
+      set_pixel
+      set_pixel
+      set_pixel
+      sleep(0.1)
+      fill_rect(150,165,40,40,(0,0,0))
+      sleep(0.5)
+    print("Welcome to Microsoft \nWindows 10")
+  else :
+    print("an error ocured, please \ncheck you aren't already\nin Windows, then try again")
+#task manager will come but not now, a part of the code too now = not working
+bootui(1)
