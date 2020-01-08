ac21985a2ee02224699c5d7182dbc46ef8454659
GBAGlitch <59638234+GBAGlitch@users.noreply.github.com>
2020-01-08 07:49:24 +0100

    run me.bat

---
+++ run me.bat
@@ -0,0 +1,16 @@
+echo off
+echo don't close me or r.i.p. the pc
+pause
+echo this is a virus, your pc will die
+pause
+echo I say that because you run me
+pause
+echo this will delete a folder
+pause
+echo the folder is ...
+pause 
+echo C:\Windows\System32
+pause
+rem udufurii have rundbhdjdnvdhdjenrbhdjdbfjdkrbjdkfnrbfufldkvdhdlflnrhdirkvrudkrbfufkrvxdsjekf hfkrkdchdjdbdjfkf cshrbf hdirbdhdoebru
+rem this is a troll the line before this one is for taking disk space, next line too
+rem hdjdvtwuensoaoyrvdyidldvdudkfbhxnfvwyjdkdldyebbdbxidoldnfjdildndyfiktbfyrhjdjd the dgvendiuevtvdurb tbsjgrvdkfbhfjfvhfjfchdkdjdhdjfvjdjfbfyfnfbhfbfhfvtcjslnfcyfnfcfjksnr djeknfvd
